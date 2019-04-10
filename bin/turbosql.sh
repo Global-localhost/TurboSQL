8615531d44f549318c9abc031818ba2a0c253b19
roberto73c <roberto.cesina@phonemetra.com>
2019-04-10 23:42:29 +0200

    Initial Commit
    Initial Commit

--- bin/turbosql.sh
+++ bin/turbosql.sh
@@ -1,1 +1,19 @@
 
+# JAVA_HOME.
+    if [ "${JAVA_HOME:-}" = "" ]; then
+        JAVA=`type -p java`
+        RETCODE=$?
+
+        if [ $RETCODE -ne 0 ]; then
+            echo $0", ERROR:"
+            echo "JAVA_HOME environment variable is not found."
+            echo "Please point JAVA_HOME variable to location of JDK 1.8 or later."
+            echo "You can also download latest JDK at http://java.com/download"
+
+            exit 1
+        fi
+
+        JAVA_HOME=
+    else
+        JAVA=${JAVA_HOME}/bin/java
+fi
